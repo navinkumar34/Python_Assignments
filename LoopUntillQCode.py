bf69fbcf0d65b374bf1017b4a1d50fc0d28843bd
navinkumar34 <61586357+navinkumar34@users.noreply.github.com>
2020-05-16 01:04:48 +0530

    Update LoopUntillQCode.py

--- LoopUntillQCode.py
+++ LoopUntillQCode.py
@@ -1,4 +1,4 @@
 # Get input from user, till user terminates the loop by entering('Q' or 'quit')
 str1 = ''
-while str1 != 'Q' and str1 != 'quit':
-    str1 = input()
+while str1 != 'q' and str1 != 'quit':
+    str1 = input().lower()
